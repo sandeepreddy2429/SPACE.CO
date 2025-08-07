d1db6dc3c2d5a048eae8088131790fccc07811f7
sandeepreddy2429 <jereddysandeep24@gmail.com>
2025-08-07 10:34:38 +0530

    from sandeep

---
+++ src/Sandeep.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Sandeep() {
+  return (
+    <div>
+        <h1>Sandeep</h1>
+        <p>Welcome to Sandeep's page!</p>
+        <p>Here you can find information about Sandeep.</p>
+        <p>Feel free to explore and learn more!</p>
+      
+    </div>
+  )
+}
