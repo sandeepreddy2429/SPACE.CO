5a5230278c588b22040658a9d15c55ca60963ca1
Praveenkumar dasari <dasaripraveenkumar2000@gmail.com>
2025-08-07 10:58:12 +0530

    modified sandeep

--- src/Sandeep.js
+++ src/Sandeep.js
@@ -6,7 +6,7 @@ export default function Sandeep() {
         <h1>Sandeep</h1>
         <p>Welcome to Sandeep's page!</p>
         <p>Here you can find information about Sandeep.</p>
-        <p>Feel free to explore and learn more!</p>
+        <p>NO way sandeep . this is worng</p>
       
     </div>
   )
